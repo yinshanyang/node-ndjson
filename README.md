8ac774f64051b55872be41d5aae79f12e620ef6b
Yin Shanyang <me@yinshanyang.com>
2018-09-06 14:44:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# `node-ndjson`
+
+Simple helper functions to read and write newline-delimited JSON files.
+
+## Functions
+
+- `parse(path)`
+- `format(path, data)`
---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const parse = (path) => fs.readFileSync(path, 'utf8').split('\n')
+  .filter((d) => d)
+  .map((d) => JSON.parse(d))
+const format = (path, data) => fs.writeFileSync(
+  path,
+  data
+    .filter((d) => d)
+    .map((d) => JSON.stringify(d)).join('\n')
+)
+
+module.exports = { parse, format }
